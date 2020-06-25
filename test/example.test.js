6f8c855b7b23b071986c2561f1e27cd871cd0768
Annalise Murphy <nali247@gmail.com>
2020-06-25 16:47:08 -0700

    1st test pass- more to come

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,22 @@
-// IMPORT MODULES under test here:
+import { add } from '../utils.js';
+
 // import example from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return 21 when passed 11 and 10', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const num1 = 11;
+    const num2 = 10;
+    const expected = 21;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = add(num1, num2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
