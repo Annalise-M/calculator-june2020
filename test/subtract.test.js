1994717f1fa9d7bd96e136ebc187bdccee020f6c
Annalise Murphy <nali247@gmail.com>
2020-06-25 17:03:08 -0700

    subtraction 3 tests pass

--- test/subtract.test.js
+++ test/subtract.test.js
@@ -17,3 +17,25 @@ test('sub - it should return 11 when passing 33 and 22', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('sub - it should return -1 when passing 14 and 15', (expect) => {
+   
+    const subStuff1 = 14;
+    const subStuff2 = 15;
+    const expected = -1;
+
+    const actual = subtract(subStuff1, subStuff2);
+
+    expect.equal(actual, expected);
+});
+
+test('sub - it should return 0 when passing 100 and 100', (expect) => {
+
+    const wtf1 = 100;
+    const wtf2 = 100;
+    const expected = 0;
+
+    const actual = subtract(wtf1, wtf2);
+    
+    expect.equal(actual, expected);
+});
